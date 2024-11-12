223215f4681d7737a4f3e63923bd76d56c2ff169
jakariamasum <jmphy698@gmail.com>
2024-11-13 02:05:13 +0600

    feat: member controller ready

--- src/app/module/member/member.controller.ts
+++ src/app/module/member/member.controller.ts
@@ -1,12 +1,80 @@
-import { Request, Response } from "express";
+import AppError from "../../errors/AppError";
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+import { MemberServices } from "./member.service";
 
-const getMember = async (req: Request, res: Response) => {
-  res.send({
+const createMember = catchAsync(async (req, res) => {
+  const result = await MemberServices.createMemberIntoDB(req.body);
+  if (!result) {
+    throw new AppError(404, "Something went wrong!");
+  }
+
+  sendResponse(res, {
     success: true,
-    message: "Test complete",
+    status: 201,
+    message: "Member created successfully",
+    data: result,
   });
-};
+});
+const getAllMembers = catchAsync(async (req, res) => {
+  const result = await MemberServices.getAllMembersFromDB();
+  if (!result) {
+    throw new AppError(404, "Something went wrong!");
+  }
+
+  sendResponse(res, {
+    success: true,
+    status: 200,
+    message: "Members retrieved successfully",
+    data: result,
+  });
+});
+const getSingleMember = catchAsync(async (req, res) => {
+  const { memberId } = req.params;
+  const result = await MemberServices.getSingleMemberFromDB(memberId);
+  if (!result) {
+    throw new AppError(404, "Something went wrong!");
+  }
+
+  sendResponse(res, {
+    success: true,
+    status: 200,
+    message: "Member retrieved successfully",
+    data: result,
+  });
+});
+const updateMember = catchAsync(async (req, res) => {
+  const { memberId } = req.params;
+  const result = await MemberServices.updateMemberIntoDB(memberId, req.body);
+  if (!result) {
+    throw new AppError(404, "Something went wrong!");
+  }
+
+  sendResponse(res, {
+    success: true,
+    status: 200,
+    message: "Member updated successfully",
+    data: result,
+  });
+});
+const deleteMembers = catchAsync(async (req, res) => {
+  const { memberId } = req.params;
+  const result = await MemberServices.deleteMemberFromDB(memberId);
+  if (!result) {
+    throw new AppError(404, "Something went wrong!");
+  }
+
+  sendResponse(res, {
+    success: true,
+    status: 200,
+    message: "Member successfully deleted",
+  });
+});
 
 export const MemberControllers = {
-  getMember,
+  createMember,
+  getAllMembers,
+  getSingleMember,
+  updateMember,
+  deleteMembers,
 };
--- src/app/module/member/member.route.ts
+++ src/app/module/member/member.route.ts
@@ -1,7 +1,5 @@
 import { Router } from "express";
-import { MemberControllers } from "./member.controller";
 
 const router = Router();
 
-router.get("/", MemberControllers.getMember);
 export const MemberRoutes = router;
