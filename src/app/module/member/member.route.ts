64e6b923c27d17e3eb6c9918d72a946c7ab3adca
jakariamasum <jmphy698@gmail.com>
2024-11-13 02:07:08 +0600

    fix: member route

--- src/app/module/member/member.route.ts
+++ src/app/module/member/member.route.ts
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import { MemberControllers } from "./member.controller";
 
 const router = Router();
+
+router.post("/", MemberControllers.createMember);
+router.get("/", MemberControllers.getAllMembers);
+router.get("/:memberId", MemberControllers.getSingleMember);
+router.put("/:memberId", MemberControllers.updateMember);
+router.delete("/:memberId", MemberControllers.deleteMembers);
 
 export const MemberRoutes = router;
