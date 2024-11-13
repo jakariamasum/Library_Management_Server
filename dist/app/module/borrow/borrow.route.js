6f59df782697b3f33b41dc0fde70324b23d4108e
jakariamasum <jmphy698@gmail.com>
2024-11-13 15:16:55 +0600

    Remove dist folder from repository and ignore it in future commits

--- dist/app/module/borrow/borrow.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BorrowRecordRoutes = void 0;
-const express_1 = require("express");
-const borrow_controller_1 = require("./borrow.controller");
-const router = (0, express_1.Router)();
-router.post("/", borrow_controller_1.BorrowRecordControllers.createBorrowRecord);
-router.get("/overdue", borrow_controller_1.BorrowRecordControllers.getOverdueBorrowList);
-exports.BorrowRecordRoutes = router;
