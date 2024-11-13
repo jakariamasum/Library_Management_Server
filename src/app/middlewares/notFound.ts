c79de2963d7592c28d8aea806943f01a7e1dc324
jakariamasum <jmphy698@gmail.com>
2024-11-13 21:34:54 +0600

    fix: not found

--- src/app/middlewares/notFound.ts
+++ src/app/middlewares/notFound.ts
@@ -7,8 +7,8 @@ import httpStatus from "http-status";
 const notFound = (req: Request, res: Response, next: NextFunction) => {
   return res.status(httpStatus.NOT_FOUND).json({
     success: false,
+    status: httpStatus.NOT_FOUND,
     message: "API Not Found !!",
-    error: "",
   });
 };
 
